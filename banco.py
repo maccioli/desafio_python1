229cdb7c4aa1f2709c2d4578ced8cc9bbc554ecc
Marcio Accioli <mjaccioli@gmail.com>
2024-04-07 19:35:49 -0300

    Resolucao do desafio concluida

--- banco.py
+++ banco.py
@@ -1,12 +1,16 @@
 menu = """
 
+## Bem vindo ao Banco Digital ##
+
+Escolha uma opção:
+
 [d] Depositar
 [s] Sacar
 [e] Extrato
 [v] Saldo
 [q] Sair
 
-=> """
+=> """.strip()
 
 saldo = 0
 limite = 500
@@ -22,23 +26,32 @@ while True:
     if opcao == "d":
         valor = float(input(f"Digite o valor a ser depositado: "))
         saldo += valor
-        print(f"Saldo atual: {saldo}")
+        extrato += f"Deposito: R${valor:.2f}\n" 
+        print(f"Saldo atual: {saldo:.2f}")
+
 
     elif opcao == "s":
-        valor = float(input(f"Digite valor a ser sacado(Limite R$500,00): "))
+        valor = float(input(f"Digite valor a ser sacado(Limite 3 saques/dia e R$500,00/saque): "))
 
-        if saldo >= valor and valor > 0:
+        if saldo >= valor and valor > 0 and valor <= limite and numero_saques <= LIMITE_SAQUES:
             saldo = saldo - valor
-            print(f"Saldo atual: {saldo}")
+            extrato += f"Saque: {valor:.2f}\n"
+            print(f"Saldo atual: {saldo:.2f}")
 
         else:
-            print("Valor não permitivo.")
+            print("Saque não permitivo, verifique o valor digitado, saldo ou limite de saques.")
+
+
+    if opcao == "e":
+        print(f"Extrato: \n{extrato}\n")
+
 
     if opcao == "v":
-        print(f"Seu saldo é de: {saldo}")
+        print(f"Seu saldo é de: {saldo:.2f}")
 
         
     elif opcao == "q":
+        print("Obrigado por usar o Banco Digital. Até a próxima!")
         break
 
     else:
